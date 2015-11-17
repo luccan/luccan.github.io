dcc027102b87cdd924986c242c304b1d94d133d6
Luccan <luccan.ryanata@gmail.com>
2015-11-17 16:15:11 +0800

    Fixed time treshold

--- js/mobileswipe.js
+++ js/mobileswipe.js
@@ -6,9 +6,9 @@ function swipedetect(el, callback){
 	startY,
 	distX,
 	distY,
-	threshold = 100, //required min distance traveled to be considered swipe
-	restraint = 80, // maximum distance allowed at the same time in perpendicular direction
-	allowedTime = 300, // maximum time allowed to travel that distance
+	threshold = 80, //required min distance traveled to be considered swipe
+	restraint = 70, // maximum distance allowed at the same time in perpendicular direction
+	allowedTime = 500, // maximum time allowed to travel that distance
 	elapsedTime,
 	startTime,
 	handleswipe = callback || function(swipedir){}
